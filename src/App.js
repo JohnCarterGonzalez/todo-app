955cefaeb6256e9805ebe9fe71dafcf914f0d758
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-08-19 11:21:12 -0700

    auto

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 
 function App() {
   const [ newItem, setNewItem ] = useState("");
-  const [items, setItems ] = useState("");
+  const [items, setItems ] = useState([]);
 
   const handleItemSubmit = (e) => {
     e.preventDefault();
+    //NOTE takes the items and sets them and adds them to the new Items array
     setItems([...items, newItem])
   };
 
   return (
     <div className="App">
       <form onSubmit={(e) => { handleItemSubmit(e); }}>
-        <input onChange={(e) => { setNewItem(e.target.value); }}/>
+        <input onChange={(e) => { setNewItem(e.target.value); }} type="text" value={newItem}/>
         <div>
           <button>Add</button>
         </div>
       </form>
-        
+      {/*map the Items to a string and display them*/}
+       {
+      items.map((todo, i) => {
+        return (
+        <div> 
+          <span>{todo}</span>
+        </div>
+        );
+      })}
     </div>
   );
 }
